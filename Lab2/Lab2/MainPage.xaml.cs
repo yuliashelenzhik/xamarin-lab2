9ed1df7176711674588d97e1efe5ea6e3cd69b95
yuliayuste <yuliashelenzhik@qq.com>
2023-08-30 16:23:50 +0200

    Initial commit

---
+++ Lab2/Lab2/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab2
+
+{
+
+    internal class MyClass : INotifyPropertyChanged
+    {
+        private string name = "Строка";
+        public string Name
+        {
+            get => name; set { name = value; OnPropertyChanged(); }
+        }
+public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+       
+    }
+}
+
